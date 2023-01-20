10ea27c01f636b9fc20d9e28c8f2ec1034a2d503
almaraubel <almar@aubel.nl>
2023-01-20 10:41:46 +0100

    line endings

--- src/Database/entrypoint.sh
+++ src/Database/entrypoint.sh
@@ -4,9 +4,9 @@ echo 'Waiting for SQL server started.';
 
 sleep 20 ;
 
-echo 'Create database..' ;
+echo 'Create database..';
 
 # Create database
 /opt/mssql-tools/bin/sqlcmd -S mymeetingsdb -d master -i /scripts/CreateDatabase_Linux.sql -U sa -P Test@12345 ;
 
-echo 'Database created' ;
+echo 'Database created';
